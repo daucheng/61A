b011d20aae1bf99a51739ff039510c20311ed1b5
Dau Cheng <frank.d.cheng@gmail.com>
2020-03-20 23:33:08 +0800

    lab04

--- test.py
+++ test.py
@@ -1,43 +1,19 @@
-def inverse_cascade(n):
-    grow(n)
-    print(n)
-    shrink(n)
+def create_row(size):
+    """Returns a single, empty row with the given size. Each empty spot is
+    represented by the string '-'.
 
-def f_then_g(f,g,n):
-    if n:
-        f(n)
-        g(n)
+    >>> create_row(5)
+    ['-', '-', '-', '-', '-']
+    """
+    "*** YOUR CODE HERE ***"
+    row_num = ['_']
+    def row(count):
+        if count == 1:
+            return row_num
+        else:
+            count = count -1
+            row_num.append('_')
+            return row(count) 
 
-          
-#grow = lambda n: f_then_g(grow, print, n//10)
-
-#shrink = lambda n: f_then_g(print, shrink, n//10)
-
-def product(m,n):
-    if n == 1:
-        return m
-    else:
-        return m + product(m,n-1)
-
-def countdown(n):
-    if n == 1:
-        print("1")
-    else:
-        print(n)
-        return countdown(n-1)
-def countup(n):
-    if n == 1:
-        print(1)
-    else:
-        countup(n-1)
-        print(n)
-
-def sum_digits(n):
-    if n // 10 == 0:
-        return n
-    else:
-        return (n%10) + sum_digits(n//10)
-
-def mystery(f,x):
-    return lambda y: f(x,y)
+    return row(size)
     
